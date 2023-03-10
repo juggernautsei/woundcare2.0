c9b69af0af56fa1c64cf0eb3abf2e6b74930ccc6
Sherwin <sherwingaddis@gmail.com>
2023-03-10 15:03:30 -0500

    error with the finding the globals

--- interface/weno/wenoconnected.php
+++ interface/weno/wenoconnected.php
@@ -8,7 +8,6 @@
  *  @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-require_once ("../globals.php");
 
 use OpenEMR\Common\Acl\AclMain;
 use OpenEMR\Rx\Weno\WenoPharmaciesJson;
